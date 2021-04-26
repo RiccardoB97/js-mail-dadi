c22feafb8379fe11fba71d439c6b6992a19a34a1
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-04-26 14:31:47 +0200

    Add style to html

--- dadi/assets/js/main.js
+++ dadi/assets/js/main.js
@@ -4,11 +4,16 @@ var playerNumber = Math.floor(Math.random() * 6) + 1;
 // Generates a random number for the computer
 var computerNumber = Math.floor(Math.random() * 6) + 1;
 
+var message = document.getElementById("final_score")
+
+document.getElementById("player_number").innerHTML = playerNumber
+document.getElementById("computer_number").innerHTML = computerNumber
+
 if (playerNumber > computerNumber) {
-    alert("You won!");
+    message.innerHTML = "Congrats, you won!";
 } else if (computerNumber > playerNumber) {
-    alert("The computer won!");
+    message.innerHTML = "Ooops, the computer won!";
 } else {
-    alert("You had the same score. It's a draw!");
+    message.innerHTML = "You had the same score. It's a draw!";
 }
 
