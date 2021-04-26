aebb09aa88c0375ca6dcb721bba7f75d4ebdb984
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-04-26 14:16:21 +0200

    Add 2 vars to generate random mnumbers for pc and player

---
+++ dadi/assets/js/main.js
@@ -0,0 +1,7 @@
+// Generates a random number for the player
+var playerNumber = Math.floor(Math.random() * 6) + 1;
+
+// Generates a random number for the computer
+var computerNumber = Math.floor(Math.random() * 6) + 1;
+
+
