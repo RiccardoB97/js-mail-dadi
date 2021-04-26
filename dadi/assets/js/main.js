1f95581834b5c04a1e78e4385367b915ac39ec82
RiccardoB97 <beraldo.riccardo@gmail.com>
2021-04-26 14:22:09 +0200

    Add if statement to compare the results and alert with the winner

--- dadi/assets/js/main.js
+++ dadi/assets/js/main.js
@@ -4,4 +4,11 @@ var playerNumber = Math.floor(Math.random() * 6) + 1;
 // Generates a random number for the computer
 var computerNumber = Math.floor(Math.random() * 6) + 1;
 
+if (playerNumber > computerNumber) {
+    alert("You won!");
+} else if (computerNumber > playerNumber) {
+    alert("The computer won!");
+} else {
+    alert("You had the same score. It's a draw!");
+}
 
